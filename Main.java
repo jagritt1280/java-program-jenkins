91520f57cce20c85210865f518e20508ea8b13b3
jagritt1280 <jagrittaneja217@gmail.com>
2025-05-09 16:48:10 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("This is java program");
+        System.out.println("Output from java program");
+        System.out.println(new Date());
+    }
+}
